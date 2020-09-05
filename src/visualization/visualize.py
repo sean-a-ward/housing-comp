6d6aded74450511d7689b1355b5340bb2108608d
Sean Ward <sean.a.ward@icloud.com>
2020-09-05 18:28:39 -0400

    Add currency function to visualize.py to format chart axes

---
+++ src/visualization/visualize.py
@@ -0,0 +1,8 @@
+
+def currency(x, pos):
+    """The two args are the value and tick position"""
+    if x >= 1e6:
+        s = '${:1.1f}M'.format(x*1e-6)
+    else:
+        s = '${:1.0f}K'.format(x*1e-3)
+    return s
